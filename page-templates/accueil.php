3f66ed66c58157410e9a589e7d2bdfbfc74eeb58
Juliette3529 <Juliette@saint-jalme.fr>
2016-09-02 14:27:22 +0200

    accueil squelette

---
+++ page-templates/accueil.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template Name: Accueil
+ *
+ * @package WordPress
+ * @subpackage prestro
+ *
+ */
+get_header();
+?>
+<div class="presentation col-md-12 col-lg-12 col-sm-12 col-xs-12">
+    
+</div>
+<div class="paralax col-md-12 col-lg-12 col-sm-12 col-xs-12">
+    
+</div>
+<div class="services col-md-12 col-lg-12 col-sm-12 col-xs-12">
+    
+</div>
+<div class="temoignages col-md-12 col-lg-12 col-sm-12 col-xs-12">
+    
+</div>
+
+<?php get_footer(); ?>
